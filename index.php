daf7e473c6ea5d6ab4e17fcf88e4cecc3e9dc6ea
Vadim <panas97@gmail.com>
2021-02-08 23:37:49 +0200

    dz1

--- index.php
+++ index.php
@@ -11,5 +11,30 @@
 
     <h1> hello Sokrates </h1>
 
+    <?php
+
+       var_dump ('20cats' + 40);
+       var_dump(3 / 1);
+       var_dump(1 / 3);
+       var_dump(21 % 2 );
+       var_dump(20 % 2 );
+
+       echo ($a=2);
+       $x = ($y = 12)-8;
+       echo $x;
+
+       var_dump(1==1.0);
+       var_dump(1===1.0);
+       var_dump('02'==2);
+       var_dump('02'===2);
+       var_dump('02'=='2');
+
+
+       $x = true xor true;
+       var_dump($x);
+
+        ?>
+
+
 </body>
 </html>
